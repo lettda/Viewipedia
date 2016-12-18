dec11e003eb3f024313fac2dae7c07342cb0321b
lettda <lettda@vcu.edu>
2016-12-18 12:55:58 -0500

    Fixed commit errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
 	
 	$(".btn-primary").click(function() {
@@ -30,38 +29,4 @@ $(document).ready(function() {
 
 		});
 	});
-
-=======
-//initialze jquery
-$(document).ready(function () {
-	//On clicking search button run the follwoing code
-	$("#search").click(function(){
-		//Pull value entered into subject search field
-		var searchSubject = $("#subjectSearch").val();
-		//Pull API URL
-		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchSubject +"&format=json&callback=?";
-
-		console.log(url);
-		
-		$.ajax({
-			type:"GET",
-			url:url,
-			async:false,
-			dataType: "json",
-			success: function(data) {
-				console.log(data[1][0]);
-				console.log(data[2][0]);
-				console.log(data[3][0]);
-
-
-
-			},
-			error: function(errorMessage){
-				aler("ERROR!");
-			}
-
-		});
-
-	});
->>>>>>> origin/master
 });
